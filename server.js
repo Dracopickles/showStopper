ef5631d1e996141c4e38227135bf9dbe19588fa7
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-12-10 18:25:19 -0800

    add user model

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+var db = require("./models");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -13,7 +22,11 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
+db.sequelize.sync(syncOptions).then(() =>
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+}));
+
+
+
+module.exports = app;
