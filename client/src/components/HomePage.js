f8fb6a8721d1945a395f8cf35c4ceaac68aa2fcd
m.peters <snakeyes721@outlook.com>
2019-12-14 13:24:22 -0800

    carousel and placeholder slides. need css

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Carousel from 'react-bootstrap/Carousel'
+
+class Registration extends Component {
+  render() {
+    return (
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ytimg.com/vi/kIcJwrQSUmY/hqdefault.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.ytimg.com/vi/6F83UKZ4_1E/hqdefault.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://mrvpn.com/wp-content/uploads/2012/10/hbo_go_logo_feature-300x175.png"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Fourth slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://eldiariony.files.wordpress.com/2018/03/download.png?w=341"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Fourth slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    )
+  }
+}
+export default Registration;
+
